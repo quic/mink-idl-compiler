cb09f0af05f37c659b3cb387f2605ba40a3b99c2
Arvind Mukund <arvimuku@quicinc.com>
2023-05-18 13:49:48 -0700

    Enable whole idl parsing with a command line AST dumper
    Signed-off-by: Arvind Mukund <arvimuku@quicinc.com>

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,34 @@ mod tests;
 #[grammar = "../grammar/idl.pest"]
 pub struct IDLParser;
 
+#[derive(clap::Parser)]
+#[command(author, version, about = None, long_about)]
+/// Parse .idl files into AST.
+///
+/// TODO(arvimuku): Currently the syntatic analysis phase is done although
+/// there's no semantic analysis done for external types. I'm wondering how i'd
+/// address the dependencies nicely especially detect cyclic dependencies and
+/// such would need to include some sort of cycle detection + topological
+/// sorting + creating a giant paste table.
+///
+/// The C-compiler for example returns `nested too deeply`, rust detects cyclic
+/// imports nicer.
+struct Args {
+    #[arg(short, long)]
+    /// Path of IDL to dump AST for
+    path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    use clap::Parser as ClapParser;
+    use pest::Parser as PestParser;
+    let args = Args::parse();
+    let file = std::fs::read_to_string(args.path).expect("File read to succeed.");
+
+    let now = std::time::Instant::now();
+    let ast = IDLParser::parse(Rule::idl, &file).expect("Successful AST dump");
+    let end = now.elapsed();
+    println!("AST generated in {end:?}");
+    std::hint::black_box(ast);
+    //dbg!(ast);
 }
