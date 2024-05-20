bf727158258426864d9ad59739d33c8d8501324e
Park, Jongyong <jongyong@quicinc.com>
2024-05-20 09:39:00 -0700

    CPP: output for const type change (#106)
    CPP Codegen: the type of const value will be `static const` rather than `#define`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,13 +2,29 @@
 
 All notable changes to this project will be documented in this file.
 
-## [0.1.1] - 2024-05-14
+## [0.1.2] - 2024-05-20
 
-### Enhancements
-- Built with statically linked musl
+### Fixes
+- CPP Codegen: Fixed the type of const value
+
+## [0.1.1] - 2024-05-14
 
 ### Fixes
 - Initialization for bundled struct
 
-### Breaking Changes
+## [0.1.0] - 2024-04-30
 
+### Enhancements
+- Object arrays implementation for Rust
+- Support off-target testing for C/CPP/Rust
+- Add support for attributes
+- #[optional] attribute adds logic to return 'Object_ERROR_INVALID' if method is undefined by the service implementor
+
+### Fixes
+- Object arrays codegen fix for C/CPP
+- Index issues of ObjectArg fix for C/CPP
+
+### Breaking Changes
+- Argument names within a method must be unique.
+- Object array needs to be bounded.
+- Struct name should not be same as any other interface name.
--- idlc_codegen_cpp/src/generator.rs
+++ idlc_codegen_cpp/src/generator.rs
@@ -1,7 +1,8 @@
 // Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 
-use idlc_codegen_c::globals::{emit_const, emit_struct};
+use idlc_codegen_c::globals::emit_struct;
+use idlc_codegen_c::types::{change_const_primitive, change_primitive};
 use idlc_mir::Node;
 
 use crate::interface::{emit_interface_impl, emit_interface_invoke};
@@ -21,7 +22,14 @@ impl idlc_codegen::SplitInvokeGenerator for Generator {
                     result.push_str(&format!("#include \"{}.hpp\"\n", inc_name));
                 }
                 Node::Const(c) => {
-                    result.push_str(&emit_const(c));
+                    let ident = c.ident.to_string();
+                    let cnt_ty = change_primitive(c.r#type);
+                    let ty = change_const_primitive(c.r#type);
+                    let value = &c.value;
+
+                    result.push_str(&format!(
+                        "static const {cnt_ty} {ident} = {ty}({value});\n\n"
+                    ));
                 }
                 Node::Struct(s) => {
                     result.push_str(&emit_struct(s.as_ref()));
